69788516358d57299faf04e8163d304613b94015
naidusaladi <79361491+naidusaladi@users.noreply.github.com>
2022-05-15 12:00:40 +0530

    Update and rename p.cpp to basic_structure_of_cpp.cpp

---
+++ basic_structure_of_cpp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+    std::cout<<"helloo";//if you don't use (using namespace std;) you have to use (std::cout<< and std::cin>>)every where 
+}
+//or
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter roll";
+    cin>>a;
+    cout<<"your roll is:"<<a;
+       
+}
